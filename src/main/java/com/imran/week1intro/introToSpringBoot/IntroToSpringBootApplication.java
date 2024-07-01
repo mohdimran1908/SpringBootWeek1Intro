374ba693a3c1488ee7e6b03bbff337a259c16842
Mohammed Imran <mohdimran1908@gmail.com>
2024-07-01 18:41:38 +0530

    1.2 Setting Up

---
+++ src/main/java/com/imran/week1intro/introToSpringBoot/IntroToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.imran.week1intro.introToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringBootApplication.class, args); 
+	}
+
+}
---
+++ src/test/java/com/imran/week1intro/introToSpringBoot/IntroToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imran.week1intro.introToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
